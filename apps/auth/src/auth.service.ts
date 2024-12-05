c9bfc44f0acf5fcebb50a47793402103c3957b61
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2024-12-05 20:21:16 +0700

    feat: auth module && add new local stratery and jwt stratery

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserDocument } from './users/models/user.schema';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import { TokenPayload } from './interfaces/token-payload.interface';
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(user: UserDocument, res: Response) {
+    const tokenPayload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+    const token = this.jwtService.sign(tokenPayload);
+
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    });
   }
+  
 }
